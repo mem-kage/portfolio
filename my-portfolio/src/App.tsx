f24285e8e333ada892fd4abc4705afaf82698095
kagehira shota <kagehira.shota.2459@members.co.jp>
2023-04-19 17:11:26 +0900

    deplou test

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          テストです
+        テストですテストですテストですテストですテストですテストですテストですテストです
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          test
+          testtesttesttesttesttest
         </a>
       </header>
     </div>
